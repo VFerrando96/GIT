fa3e81b960c5a09b6b0e9741c79b936416bb0fc8
Ferrando <victor.drone2013@gmail.com>
2023-10-18 17:25:53 -0300

    descomentado a funçaõ princiapl

--- Boletim_focus/main.py
+++ Boletim_focus/main.py
@@ -1,11 +1,11 @@
 import Functions as f 
 
-# resultado = f.abrir_site_e_buscar_data_publicacao_Recente()
-# dia_hoje=f.Dia_de_hoje()
+resultado = f.abrir_site_e_buscar_data_publicacao_Recente()
+dia_hoje=f.Dia_de_hoje()
 
-# if rf'Data de publicação: {dia_hoje}'  in resultado:
-#     print('Boletim Saiu hoje')
-#     f.Baixar_arquivo(f.D_3(dia_hoje))
+if rf'Data de publicação: {dia_hoje}'  in resultado:
+    print('Boletim Saiu hoje')
+    f.Baixar_arquivo(f.D_3(dia_hoje))
 
 
 ################################################################### Criar Historico #########################################################
