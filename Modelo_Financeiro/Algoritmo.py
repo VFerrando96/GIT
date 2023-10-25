1e4639a8260bc026f4e927eb38bfb31fad10bbb3
Ferrando <victor.drone2013@gmail.com>
2023-10-24 21:02:40 -0300

    retorno das ações

--- Modelo_Financeiro/Algoritmo.py
+++ Modelo_Financeiro/Algoritmo.py
@@ -4,5 +4,11 @@ import quantstats as qs
 
 dados = pd.read_csv('Modelo_Financeiro/dados_empresas (1).csv')
 dados=dados[dados['volume_negociado']>1000000]
-valores_unicos = dados['ticker'].unique()
-print(valores_unicos)
+# print(dados)
+#####################################################################   
+#                       Calculo Retorno Mensal                      #
+#####################################################################  
+dados['retorno'] = dados.groupby('ticker')['preco_fechamento_ajustado'].pct_change()
+dados['retorno']= dados['retorno'].shift(-1)
+print(dados)
+
