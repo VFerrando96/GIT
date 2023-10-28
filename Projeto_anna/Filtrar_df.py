a5a82a6cf52137d1f50a9269cec5dffe53e85571
Victor Ferrando <victor.ferrando@gmail.com>
2023-10-28 14:40:04 -0300

    Alteracoes feitas e concluidas

--- Projeto_anna/Filtrar_df.py
+++ Projeto_anna/Filtrar_df.py
@@ -1,1 +1,26 @@
 import pandas as pd
+import numpy as np
+import random
+import faker
+# Dados=pd.read_csv('Seuarquivo.csv')
+data = []
+fake = faker.Faker()
+for i in range(20):
+    data.append({
+        'id': i + 1,
+        'codigo do cliente': random.randint(1000, 9999),
+        'nome': fake.name(),
+        'email': fake.email()
+    })
+
+# Criando o DataFrame
+df = pd.DataFrame(data)
+
+# Exibindo o DataFrame
+
+
+df['email']=df['email'].replace(df['email'][0],'victor.ferrando@gmail.com')
+df['email']=df['email'].replace(df['email'][1],'victor.ferrando@gmail.com')
+df['email']=df['email'].drop_duplicates()
+df = df.dropna(subset=['email'])
+print(df)
