16a7dc3e00aea2bfc5cf3a169aeb14b2ddcb3ddf
Ferrando <victor.drone2013@gmail.com>
2023-10-30 19:01:48 -0300

    Alteração para funcionamento do codigo

--- Modelo_Financeiro/Meta.py
+++ Modelo_Financeiro/Meta.py
@@ -8,13 +8,9 @@ driver = webdriver.Chrome()
 
 driver.get('https://www.fundamentus.com.br/resultado.php')
 local_tabela = '/html/body/div[1]/div[2]/table'
-
 tabela = driver.find_element('xpath', local_tabela)
-
 html_tabela = tabela.get_attribute('outerHTML')
-
 tabela = pd.read_html(StringIO(html_tabela), thousands='.', decimal=',')[0]
-
 tabela = tabela.set_index('Papel')
 tabela = tabela[['Cotação', 'EV/EBIT', 'ROIC', 'Liq.2meses']]
 tabela['ROIC'] = tabela['ROIC'].str.replace("%", "")
@@ -27,19 +23,19 @@ tabela = tabela[tabela['ROIC'] > 0]
 tabela['ranking_ev_ebit'] = tabela['EV/EBIT'].rank(ascending = True)
 tabela['ranking_roic'] = tabela['ROIC'].rank(ascending = False)
 tabela['ranking_final'] = tabela['ranking_ev_ebit'] + tabela['ranking_roic']
-
 tabela = tabela.sort_values('ranking_final')
-
 tabela=tabela.head(10)
 tickers=tabela.index
-
-mt5.initialize(login=53477444, server="XPMT5-DEMO",password="G99bfaT#")
+#######################################################################################################################################################################################
+#   Iniciando o Meta Trader 5                                                                                                                                                         #  
+#######################################################################################################################################################################################
+mt5.initialize(login=53477444, server="XPMT5-DEMO",password="899513Vi!")
 # shut down connection to the MetaTrader 5 terminal
 
 for acao in tickers:
     mt5.symbol_select(acao)
     preco=mt5.symbol_info(acao).ask
-    quantidade=100.0
+    quantidade=1.0
     ordem_compra = {
             "action": mt5.TRADE_ACTION_DEAL,
             "symbol": acao,
@@ -53,6 +49,3 @@ for acao in tickers:
     }
     compra = mt5.order_send(ordem_compra)
     print(compra)
-    
-
-mt5.shutdown()
