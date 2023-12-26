d57d099c1423754c1aea85e6f6413bcb011f1e68
VFERRANDO96 <victor.ferrando@gmail.com>
2023-12-26 11:18:13 -0300

    Nova Pasta "Base"

--- Informacoes_DFP/Baixar_extrair.py
+++ Informacoes_DFP/Baixar_extrair.py
@@ -1,6 +1,6 @@
 import Functions as fx
 
-# anos=range(2010,2023)
-# for ano in anos:
-#     fx.Baixar_dados_abertos(ano)
-fx.Descompactar_DRE_ano(2022,'00.000.000/0001-91')
+anos=range(2010,2023)
+for ano in anos:
+    fx.Baixar_dados_abertos(ano)
+    fx.Descompactar_arquivos_completo_zip()
